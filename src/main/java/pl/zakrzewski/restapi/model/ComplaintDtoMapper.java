9fe64cb70a76dc273e1c63d7e0f296f2a99d4d2b
Przemyslaw Zakrzewski <p.zakrzewski94@gmail.com>
2025-05-27 00:02:41 +0200

    Changed from Collectors to Stream

--- src/main/java/pl/zakrzewski/restapi/model/ComplaintDtoMapper.java
+++ src/main/java/pl/zakrzewski/restapi/model/ComplaintDtoMapper.java
@@ -3,7 +3,6 @@ package pl.zakrzewski.restapi.model;
 import pl.zakrzewski.restapi.model.dto.ComplaintDto;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ComplaintDtoMapper {
 
@@ -14,7 +13,7 @@ public class ComplaintDtoMapper {
     public static List<ComplaintDto> mapToComplaintDtos(List<Complaint> complaints) {
         return complaints.stream()
                 .map(complaint -> mapToComplaintDto(complaint))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     public static ComplaintDto mapToComplaintDto(Complaint complaint) {
